70bc4a8780bd3a8740dd1c20e11a4d4f23a35f43
fathurifki <rifkifathu@gmail.com>
2020-03-05 10:47:42 +0700

    add instalation kops kubectl aws-cli

---
+++ kops.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+//Install Kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
+sudo apt-get update
+curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops-linux-amd64
+sudo mv kops-linux-amd64 /usr/local/bin/kops
+sudo apt-get update
+sudo apt-get install awscli
+aws configure
