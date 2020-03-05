21c7759d9071ee6f35367c0364811bc422473890
fathurifki <rifkifathu@gmail.com>
2020-03-05 10:58:39 +0700

    add constructor for setting bucket & cluster

--- kops.bash
+++ kops.bash
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-//Install Kubectl
+echo 'Install Kubectl'
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 kubectl version --client
 sudo apt-get update
+echo 'Install Kops'
 curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops-linux-amd64
 sudo mv kops-linux-amd64 /usr/local/bin/kops
 sudo apt-get update
+echo 'Install Aws-CLI'
 sudo apt-get install awscli
 aws configure
+
+export bucket_name={YOUR_BUCKET_NAME}
+export KOPS_CLUSTER_NAME={YOUR_KOPS_CLUSTER_NAME}
+export KOPS_STATE_STORE=s3://${bucket_name}
+
