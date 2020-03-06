954a949feb2a96599214c6d0fa5e17121c2b77c8
fathurifki <rifkifathu@gmail.com>
2020-03-06 23:09:41 +0700

    initiate docker

--- kops.bash
+++ kops.bash
@@ -13,9 +13,15 @@ sudo mv kops-linux-amd64 /usr/local/bin/kops
 sudo apt-get update
 echo 'Install Aws-CLI'
 sudo apt-get install awscli -y
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
 aws configure
 
-
+'echo export to BASHRC'
 export bucket_name={YOUR_BUCKET_NAME}
 export KOPS_CLUSTER_NAME={YOUR_KOPS_CLUSTER_NAME}
 export KOPS_STATE_STORE=s3://${bucket_name}
